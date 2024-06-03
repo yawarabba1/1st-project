45243545c1eb88419f8eff73420e73eb53612ca0
yawarabbasi <yawarabbasi570@gmail.com>
2024-06-03 09:27:07 +0500

    Today Practice

---
+++ New folder/script.js
@@ -0,0 +1,84 @@
+// First Swiper code start
+var swiper = new Swiper(".swiper", {
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    hide: true,
+  },
+});
+// First Swiper code end
+// Second Swiper code start
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Second Swiper code end
+// Third Swiper code start
+var swiper = new Swiper(".swiper3", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Third Swiper code end
+// Fourth Swiper code start
+var swiper = new Swiper(".swiper4", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Fourth Swiper code end
+// Fifth Swiper code start
+var swiper = new Swiper(".swiper5", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Fifth Swiper code end
+// Sixth Swiper code start
+var swiper = new Swiper(".swiper6", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Sixth Swiper code end
+// Seventh Swiper code start
+var swiper = new Swiper(".swiper7", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Seventh Swiper code end
+// Eighth Swiper code start
+var swiper = new Swiper(".swiper8", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  autoplay: {
+    delay: 2800,
+    disableOnInteraction: false,
+  },
+});
+// Eighth Swiper code end
